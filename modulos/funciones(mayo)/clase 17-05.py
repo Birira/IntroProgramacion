94a4cd9daf5e8fc78609751f34ad98a64d8020b8
Vicente <vicente.pereira.ch@gmail.com>
2023-08-25 20:18:49 -0400

    actualizacion introProgra

--- modulos/funciones(mayo)/clase 17-05.py
+++ modulos/funciones(mayo)/clase 17-05.py
@@ -1,5 +1,4 @@
-def esprimo(num):
-
+def esPrimo(num):
     for d in range(2,num):
         if num % d == 0:
             return False
@@ -8,11 +7,10 @@ def esprimo(num):
 def primos(lista):
     contador = 0
     for n in lista:
-        if esprimo(n):
+        if esPrimo(n):
             contador += 1
 
     return contador
 
 lista = [1,7,8,5,13]
 print(primos(lista))
-
